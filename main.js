92378559392c7eade48e6c35bf8f9370b159079f
Arjun1932010 <83018389+Arjun1932010@users.noreply.github.com>
2022-06-29 18:53:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,23 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
 }
 function draw(){
-
+   image(video,0,0,300,300);
 }
 function take_snapshot(){
     save("mustache_image.png");
+}
+function modelLoaded(){
+    console.log("Got pose");
+}
+function gotPoses(results){
+    console.log(results);
+    console.log("nose_x ="+ results[0].pose.nose.x);
+    console.log("nose_y ="+ results[0].pose.nose.y);
 }
